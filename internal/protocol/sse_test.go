1030f2c2cce5e2efdf427b61dea90b7e1ed9e5b6
mrgolftech <38007652+mrgolftech@users.noreply.github.com>
2026-09-22 07:09:57 +0800

    fix(protocol): correct SSE fixture escapes

--- internal/protocol/sse_test.go
+++ internal/protocol/sse_test.go
@@ -7,21 +7,21 @@ import (
 )
 
 func TestSSEScanner(t *testing.T) {
-	input := ": heartbeat\\n" +
-		"data: {\\\"type\\\":\\\"content_block_delta\\\"}\\n\\n" +
-		"event: ignored-name\\n" +
-		"data: line-one\\n" +
-		"data: line-two\\n\\n"
+	input := ": heartbeat\n" +
+		"data: {\"type\":\"content_block_delta\"}\n\n" +
+		"event: ignored-name\n" +
+		"data: line-one\n" +
+		"data: line-two\n\n"
 
 	s := NewSSEScanner(strings.NewReader(input))
 	first, err := s.Next()
 	if err != nil { t.Fatal(err) }
-	if first != "{\\\"type\\\":\\\"content_block_delta\\\"}" {
+	if first != "{\"type\":\"content_block_delta\"}" {
 		t.Fatalf("unexpected first event: %q", first)
 	}
 	second, err := s.Next()
 	if err != nil { t.Fatal(err) }
-	if second != "line-one\\nline-two" {
+	if second != "line-one\nline-two" {
 		t.Fatalf("unexpected multiline event: %q", second)
 	}
 	if _, err := s.Next(); err != io.EOF {
